070feb981aaa402981e41a159f6115a2e212c9a6
vishnu shaji <93571312+vishnu-shaji@users.noreply.github.com>
2022-06-10 15:45:36 +0530

    Add files via upload

---
+++ DBMS -lab/main.sql
@@ -0,0 +1,6 @@
+
+create table employee (emp_name varchar(20), department varchar(20), salary int);
+
+insert into employee values ("arun","cs","20000");
+insert into employee values ("rahul","phy","-20000");
+select * from employee;
